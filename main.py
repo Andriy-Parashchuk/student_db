5ec2bd486e101649dcf677888f2f43a018be0d03
Andriy-Parashchuk <andreiparaschyk1401@gmail.com>
2025-05-03 16:59:10 +0300

    Add student-course Many-To-Many relation

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def init_db():
         cursor.executescript('''
         DROP TABLE IF EXISTS students;
         DROP TABLE IF EXISTS courses;    
+        DROP TABLE IF EXISTS students_courses;
+        PRAGMA foreign_keys = ON;
         
         CREATE TABLE students(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,6 +22,17 @@ def init_db():
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name VARCHAR(100)
         );
+        
+        CREATE TABLE students_courses(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            student_id INTEGER,
+            course_id INTEGER,
+            
+            FOREIGN KEY (student_id) REFERENCES students(id),
+            FOREIGN KEY (course_id) REFERENCES courses(id)
+        );
+        
+        
         
         ''')
         connection.commit()
